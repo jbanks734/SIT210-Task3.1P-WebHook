52a7136eed06b6a52c86684b6d50c0d84599a488
James Banks <jbanks@hey.com>
2022-03-22 13:20:17 +1100

    Clean up code

--- src/WebHook.ino
+++ src/WebHook.ino
@@ -1,7 +1,5 @@
 #include "Adafruit_DHT.h"
 
-int led = D7; // The on-board LED
-
 #define DHTPIN 2 // what pin we're connected to
 
 #define DHTTYPE DHT11 // DHT 11
@@ -10,10 +8,8 @@ DHT dht(DHTPIN, DHTTYPE);
 
 void setup()
 {
-  pinMode(led, OUTPUT);
   Serial.begin(9600);
   Serial.println("DHTxx test!");
-
   dht.begin();
 }
 
