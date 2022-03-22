057e51b3004b3e176cf34c6eb29e044b78ae4f17
James Banks <jbanks@hey.com>
2022-03-22 12:40:36 +1100

    Connected to Web

--- src/WebHook.ino
+++ src/WebHook.ino
@@ -1,18 +1,39 @@
+#include "Adafruit_DHT.h"
+
 int led = D7; // The on-board LED
+
+#define DHTPIN 2 // what pin we're connected to
+
+#define DHTTYPE DHT11 // DHT 11
+
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup()
 {
   pinMode(led, OUTPUT);
+  Serial.begin(9600);
+  Serial.println("DHTxx test!");
+
+  dht.begin();
 }
 
 void loop()
 {
-  digitalWrite(led, HIGH); // Turn ON the LED
+  delay(10000);
 
-  String temp = String(random(60, 80));
-  Particle.publish("temp", temp, PRIVATE);
-  delay(30000); // Wait for 30 seconds
+  float t = dht.getTempCelcius();
 
-  digitalWrite(led, LOW); // Turn OFF the LED
-  delay(30000);           // Wait for 30 seconds
+  Serial.print("Temp: ");
+  Serial.println(t);
+
+  String temp = String(t);
+
+  if (temp == "nan")
+  {
+    Particle.publish("temp", temp, PRIVATE);
+  }
+  else
+  {
+    Serial.println("nan");
+  }
 }
